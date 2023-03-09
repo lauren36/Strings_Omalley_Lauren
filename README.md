d682bdc652f4d6f11e5db2eefe10d06e79276251
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-03-09 09:12:14 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Strings_Omalley_Lauren
+ 
---
+++ Strings_Omalley_Lauren/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Strings_Omalley_Lauren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks the user to input a character then assigns the input to a variable
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks if the character that the user entered is a letter
+            bool isLetter = Char.IsLetter(myChar);
+
+            // Shows the results to the console
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}.");
+
+            // Asks the user a question and then saves the response to a varialbe called sentence
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            // Asks the user to enter a word then saves the response to a variable called word
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            // Checks if the word is in the sentence
+            bool isIn = sentence.Contains(word);
+
+            // Shows if the word is in the sentence
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
+        }
+    }
+}
